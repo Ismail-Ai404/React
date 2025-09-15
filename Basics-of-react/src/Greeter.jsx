b2b35494702abbf00d18415d1e317dab96c1bade
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-09-15 18:06:57 +0600

    Add vite to react skeletan and learn about props

---
+++ Basics-of-react/src/Greeter.jsx
@@ -0,0 +1,6 @@
+// export default function Greeter(props) {
+//   return <h1>Hi There, {props.person}</h1>;
+// }
+export default function Greeter({person}) {
+  return <h1>Hi There, {person}</h1>;
+}
